5cf4ef26174de26ee20671d795214e1c4acae878
sz <114493271+1535316910@users.noreply.github.com>
2023-04-06 18:51:21 +0800

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world你好");
-console.log("hello world你好");
-console.log("hello world你好");
-console.log("hello world你好");
-console.log("hello world你好");
-console.log("hello world你好");
-console.log("111");
